07c12644ead5e73cd5f36e3ce5bd737d45293208
DiMM2021 <momentservice@gmail.com>
2022-09-30 16:36:30 +0600

    First deployment to production

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+AdminUser.create!(email: 'admin@maestro.com', password: 'maestro', password_confirmation: 'maestro')
+
 if Rails.env.development?
   AdminUser.create!(email: 'admin@example.com', password: 'password',
                     password_confirmation: 'password')
 end
+
+
